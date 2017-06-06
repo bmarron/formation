40054c6cbe036c7f8d2f103b8757c86c46ca1fe4
LiveEstabueno <contact@estabueno.com>
2017-06-06 12:34:41 +0200

    CHAmnet ligne 6

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 	<title>La page d'accueil de notre projet commun</title>
 </head>
 <body>
-Page d'accueil pour traiter la us54
+Page d'accueil pour traiter la us54 à la recherche
+test conflit
 </body>
 </html>
