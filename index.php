9aa834eba79baead866d3efa90401c47d36b4711
Aurelie Antonio <aurelie.antonio@ens-lyon.fr>
2017-10-30 14:42:12 +0100

    Modification index.php pour faire du menage dans le texte

--- index.php
+++ index.php
@@ -3,9 +3,6 @@
 	<title>La page d'accueil de notre projet commun</title>
 </head>
 <body>
-Page d'accueil pour traiter la us54 à la recherche
-test conflit
-Page d'accueil pour traiter la us54 encore une modification
 <a href="list.php">Liste des stagiaires</a>
 </body>
 </html>
