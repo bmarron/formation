314947bc5e88237eb6b2a0eee42daac1fdc399a6
bmarron <bmarron@dawan.fr>
2017-10-30 14:43:34 +0100

    modif index.php

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <html>
 <head>
 	<title>La page d'accueil de notre projet commun</title>
+<link type="stylesheet/css" href="style.css"/>
 </head>
 <body>
-Page d'accueil pour traiter la us54 à la recherche
+Page d'accueil de l'application
 test conflit
 Page d'accueil pour traiter la us54 encore une modification
 <a href="list.php">Liste des stagiaires</a>
