d02dba63b510e9987f85422beb8698d5ced75677
Benjamin MARRON <bmarron@dawan.fr>
2018-10-17 16:27:39 +0200

    Bug745 : correction libelle sur page accueil

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 Page d'accueil pour traiter la us54 à la recherche
 test conflit
 Page d'accueil pour traiter la us54 encore une modification
+Correction bug 745
 <a href="list.php">Liste des stagiaires</a><br/>
 <a href="edit.php">Edition d'un stagiaire</a>
 </body>
