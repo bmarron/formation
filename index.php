7770cbf98c556d83ac15b6cb1775fe87ef7b5680
Geo13000 <sg.one.6z@gmail.com>
2018-10-17 16:27:39 +0200

    Correction du méchant bug

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
 <body>
 Page d'accueil pour traiter la us54 à la recherche
 test conflit
-Page d'accueil pour traiter la us54 encore une modification
+Page d'accueil pour traiter la us54 encore une modification<br/>
+Correction du méchant bug
 <a href="list.php">Liste des stagiaires</a><br/>
 <a href="edit.php">Edition d'un stagiaire</a><br/>
 <p>Ceci concerne les stagiaires</p>
